414a6a01a9e71c88733189b458175ac66db487c7
João Carlos <joaocpinheiro03@gmail.com>
2023-08-23 19:49:30 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
           <Header />
           <main className="AppBody">
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
               <Route path="login/*" element={<Login />} />
               <Route
                 path="conta/*"
